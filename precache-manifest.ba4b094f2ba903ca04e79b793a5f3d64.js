8e41db035fa52d9b635f39bd39436f83d7f971ca
Magform <nicolas25.02.2003.nf@gmail.com>
2021-04-16 11:45:49 +0200

    Updates

---
+++ precache-manifest.ba4b094f2ba903ca04e79b793a5f3d64.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd8335c91f9648e1f2e589efa5378f3",
+    "url": "/nicolasferraresso/index.html"
+  },
+  {
+    "revision": "14c7d346c0f9474d5f72",
+    "url": "/nicolasferraresso/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "b5bfadaaa87c2a4ccefe",
+    "url": "/nicolasferraresso/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "3040178f1454765f0199",
+    "url": "/nicolasferraresso/static/css/main.b4a8af0a.chunk.css"
+  },
+  {
+    "revision": "7714bdf713198d9d4e6a",
+    "url": "/nicolasferraresso/static/js/2.9f41c84a.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/nicolasferraresso/static/js/2.9f41c84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c7d346c0f9474d5f72",
+    "url": "/nicolasferraresso/static/js/3.43070133.chunk.js"
+  },
+  {
+    "revision": "b5bfadaaa87c2a4ccefe",
+    "url": "/nicolasferraresso/static/js/4.fe942e71.chunk.js"
+  },
+  {
+    "revision": "3040178f1454765f0199",
+    "url": "/nicolasferraresso/static/js/main.8798f9cd.chunk.js"
+  },
+  {
+    "revision": "c06fe8da2947e418c5f5",
+    "url": "/nicolasferraresso/static/js/runtime-main.7fd0b358.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/nicolasferraresso/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/nicolasferraresso/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/nicolasferraresso/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/nicolasferraresso/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8c41f85e71b94503d866831667add90f",
+    "url": "/nicolasferraresso/static/media/liceoLogo.8c41f85e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/nicolasferraresso/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/nicolasferraresso/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/nicolasferraresso/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
